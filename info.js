52a3acb98930936cd41ed16ccaf81aefc04fa49f
AnDeRSson3 <103232122+AnDeRSson3@users.noreply.github.com>
2024-02-07 21:36:55 -0800

    Add files via upload

---
+++ info.js
@@ -0,0 +1,111 @@
+AFRAME.registerComponent("info-banner", {
+  schema: {
+    itemId: { default: "", type: "string" },
+  },
+  update: function () {
+    this.createBanner();
+  },
+  createBanner: function () {
+    postersInfo = {
+      superman: {
+        banner_url: "supermanposter.jpg",
+        title: "Superman",
+        released_year: "1983",
+        description:
+          "Superman is an ongoing American comic book series featuring the DC Comics superhero Superman as its main protagonist. Superman began as one of several anthology features in the National Periodical Publications comic book Action Comics in June 1938. The strip proved so popular that National launched Superman into his own self-titled comic book, the first for any superhero, premiering with the cover date Summer 1939.",
+      },
+      spiderman: {
+        banner_url: "spidermanposter.jpg",
+        title: "Spiderman",
+        released_year: "1962",
+        description:
+          "Spider-Man is a fictional superhero created by writer-editor Stan Lee and writer-artist Steve Ditko. He first appeared in the anthology comic book Amazing Fantasy (Aug. 1962) in the Silver Age of Comic Books.",
+      },
+      "mfdoom": {
+        banner_url: "mfdoom.jpg",
+        title: "MF DOOM",
+        released_year: "1997",
+        description:
+          "In 1997, Dumile began freestyling incognito at open-mic events at the Nuyorican Poets Café in Manhattan, obscuring his face by putting tights over his head. He turned this into a new identity, MF Doom, with a mask similar to that of Marvel Comics supervillain Doctor Doom.",
+      },
+      "batman": {
+        banner_url: "batmanposter.jpg",
+        title: "Batman",
+        released_year: "1939",
+        description:
+          "Batman's origin story features him swearing vengeance against criminals after witnessing the murder of his parents Thomas and Martha as a child, a vendetta tempered with the ideal of justice. He trains himself physically and intellectually, crafts a bat-inspired persona, and monitors the Gotham streets at night.",
+      },
+    };
+    const { itemId } = this.data;
+
+    const fadeBackgroundEl = document.querySelector("#fadeBackground");
+
+    const entityEl = document.createElement("a-entity");
+    entityEl.setAttribute("visible", true);
+    entityEl.setAttribute("id", `${itemId}-banner`);
+
+    entityEl.setAttribute("geometry", {
+      primitive: "plane",
+      width: 0.9,
+      height: 1,
+    });
+
+    entityEl.setAttribute("material", { color: "#000" });
+    entityEl.setAttribute("position", { x: 0, y: 0.1, z: -1 });
+
+    const item = postersInfo[itemId];
+
+    const imageEl = this.createImageEl(item);
+    const titleEl = this.createTitleEl(item);
+    const descriptionEl = this.createDescriptionEl(item);
+
+    entityEl.appendChild(imageEl);
+    entityEl.appendChild(titleEl);
+    entityEl.appendChild(descriptionEl);
+
+    fadeBackgroundEl.appendChild(entityEl);
+  },
+  createImageEl: function (item) {
+    const entityEl = document.createElement("a-entity");
+    entityEl.setAttribute("visible", true);
+    entityEl.setAttribute("geometry", {
+      primitive: "plane",
+      width: 0.85,
+      height: 0.4,
+    });
+    entityEl.setAttribute("material", { src: item.banner_url });
+    entityEl.setAttribute("position", { x: 0, y: 0.3, z: 0.05 });
+    return entityEl;
+  },
+  createTitleEl: function (item) {
+    const entityEl = document.createElement("a-entity");
+    entityEl.setAttribute("visible", true);
+    entityEl.setAttribute("text", {
+      shader: "msdf",
+      anchor: "left",
+      font: "https://cdn.aframe.io/examples/ui/Viga-Regular.json",
+      width: 1.2,
+      height: 2,
+      color: "#fff",
+      value: `${item.title} (${item.released_year})`,
+    });
+    entityEl.setAttribute("position", { x: -0.4, y: 0.02, z: 0.05 });
+    return entityEl;
+  },
+  createDescriptionEl: function (item) {
+    const entityEl = document.createElement("a-entity");
+    entityEl.setAttribute("visible", true);
+    entityEl.setAttribute("text", {
+      shader: "msdf",
+      anchor: "left",
+      font: "https://cdn.aframe.io/examples/ui/Viga-Regular.json",
+      width: 0.75,
+      height: 2,
+      color: "#fff",
+      wrapCount: "40",
+      value: item.description,
+    });
+    entityEl.setAttribute("position", { x: -0.4, y: -0.24, z: 0.05 });
+    return entityEl;
+  },
+});
